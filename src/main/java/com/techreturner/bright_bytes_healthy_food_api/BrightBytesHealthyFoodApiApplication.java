3ce859da30d2edde9850ef60fe262c47a9fd2ec0
Hin Chi WONG <phantomcase@gmail.com>
2022-02-24 23:34:06 +0000

    Create Project Base With Spring.io

---
+++ src/main/java/com/techreturner/bright_bytes_healthy_food_api/BrightBytesHealthyFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.techreturner.bright_bytes_healthy_food_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrightBytesHealthyFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrightBytesHealthyFoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techreturner/bright_bytes_healthy_food_api/BrightBytesHealthyFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techreturner.bright_bytes_healthy_food_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrightBytesHealthyFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
